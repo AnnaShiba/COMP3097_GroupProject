64156cffb5944063d7dc0ba29eff52bcf684dc75
harrybandukda <harrisbandukda@gmail.com>
2025-04-14 07:35:41 -0700

    added more data

---
+++ recipe-app/RecipeDetailView.swift
@@ -0,0 +1,18 @@
+//
+//  RecipeDetailView.swift
+//  recipe-app
+//
+//  Created by harry on 25/01/1404 AP.
+//
+
+import SwiftUI
+
+struct RecipeDetailView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    RecipeDetailView()
+}
